d791a0ce2d322afeb27def445b4552488f9c192d
siu <francisco.rodriguez@iesnervion.es>
2024-03-25 19:05:33 +0100

    Un peo

---
+++ app/src/main/java/com/example/regalanavidad/modelos/SitioRecogida.kt
@@ -0,0 +1,10 @@
+package com.example.regalanavidad.modelos
+
+data class SitioRecogida(
+    var nombreSitio : String = "",
+    var latitudSitio : Double = 0.0,
+    var longitudSitio : Double = 0.0,
+    
+) {
+
+}
